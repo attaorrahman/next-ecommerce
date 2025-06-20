cac699710263aec931b7161db0de9049adf1a0ba
attaorrahman <malikattaurahman@gmail.com>
2025-06-20 11:44:42 +0500

    installing next.js and tailwind css

---
+++ app/(main)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-red-400 text-3xl font-bold'>This is Home Page</h1>
+    </div>
+  )
+}
+
+export default page
